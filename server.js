b73b8a120e2efc8b9ee06ddf0e3f4d6ec4cf7528
Meko Steinberg <steinberg.meko@outlook.com>
2022-11-17 21:52:07 -0800

    setup envir.variables

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
+const db = mongoose.connection;
+const methodOverride = require("method-override");
+const bookRouter = require("./routes/routes");
 
 require('dotenv').config()
 const mongoURI = process.env.MONGO_URI
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+
+// app.use(bookRouter);
+
+mongoose.connect(mongoURI, () => {
+    console.log("Database is connected");
+  });
+
+app.listen(3000, () => console.log("Reading on port 3000"));
